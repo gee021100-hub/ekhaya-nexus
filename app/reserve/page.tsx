67502579e14d52d470d8836d5f2cfd771390403b
Brian Kayamba <brianbwailakayamba@gmail.com>
2026-09-09 09:55:04 +0200

    remove nexus skeleton files, set up platform foundation

--- app/reserve/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function ReservePage() {
-  redirect('/reserve/players');
-}
--- app/senior/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function SeniorPage() {
-  redirect('/senior/standings');
-}
